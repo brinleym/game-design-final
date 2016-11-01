f9ff0c20f0407edb0dce075b41b64628824fec79
Benjamin Fuligni <Benjamin Fuligni>
2016-11-01 18:20:30 -0400

    add

--- tim_stuff/scenes/UfoTutorial/Assets/Scripts/GameController.cs
+++ tim_stuff/scenes/UfoTutorial/Assets/Scripts/GameController.cs
@@ -7,8 +7,8 @@ public class GameController : MonoBehaviour {
 	public Vector2 playerInitPos;
 	public GameObject player2;
 	public Vector2 player2InitPos;
-	//public GameObject player3;
-	//public Vector2 player3InitPos;
+	public GameObject player3;
+	public Vector2 player3InitPos;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +16,12 @@ public class GameController : MonoBehaviour {
 		//spawn players
 		Vector2 playerSpawnPosition = playerInitPos;
 		Vector2 player2SpawnPosition = player2InitPos;
-		//Vector2 player3SpawnPosition = player3InitPos;
+		Vector2 player3SpawnPosition = player3InitPos;
 		Quaternion spawnRotation = Quaternion.identity;
 
 		Instantiate (player, playerSpawnPosition, spawnRotation);
 		Instantiate (player2, player2SpawnPosition, spawnRotation);
-		//Instantiate (player3, player3SpawnPosition, spawnRotation);
+		Instantiate (player3, player3SpawnPosition, spawnRotation);
 
 	}
 
---
+++ tim_stuff/scenes/UfoTutorial/Assets/Scripts/Player3Controller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player3Controller : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // player movement code (working)
+        //transform.position += transform.right *Input.GetAxis(axisName)* speed;
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            			Vector3 position = this.transform.position;
+            			position.x--;
+            			this.transform.position = position;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+
+    }
+}
