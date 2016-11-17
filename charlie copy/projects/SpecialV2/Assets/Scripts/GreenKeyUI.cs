34c14c333ff9438427fb77530f51c922628f79be
Charles Yang <charles.yang@tufts.edu>
2016-11-17 17:14:39 -0500

    Copy with working UI + scripts for UI.

---
+++ charlie copy/projects/SpecialV2/Assets/Scripts/GreenKeyUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GreenKeyUI : MonoBehaviour {
+	Image myImageComponent;
+	public Sprite haveKey;
+	public Sprite noKey;
+	// Use this for initialization
+	void Start () {
+		myImageComponent = GetComponent<Image> ();
+		SetNoKey();
+	}
+
+
+	public void SetNoKey() //method to set our first image
+	{
+		myImageComponent.sprite = noKey;
+	}
+
+	public void SetHaveKey(){
+		myImageComponent.sprite = haveKey;
+	}
+}
