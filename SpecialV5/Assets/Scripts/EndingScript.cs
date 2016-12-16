8e2de5d03578932f7c8a2ba573a176969fc93384
Tim Ho <tcho419@gmail.com>
2016-12-16 17:20:11 -0500

    final version

---
+++ SpecialV5/Assets/Scripts/EndingScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndingScript : MonoBehaviour {
+
+	public Sprite image1, image2, image3;
+	private Image currentImage;
+	float startTime, curTime;
+
+	void Start () {
+		currentImage = GetComponent<Image>();
+		currentImage.sprite = image1;
+		startTime = Time.time;
+	}
+
+	void Update()
+	{
+		curTime = Time.time;
+		if (curTime - startTime > 6)
+			currentImage.sprite = image3;
+		else if (curTime - startTime > 3)
+			currentImage.sprite = image2;
+	}
+}
